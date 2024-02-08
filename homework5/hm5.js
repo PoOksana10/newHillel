96c3252805564ece64fa3ebda96d282adb5fea5f
PoOksana10 <129370380+PoOksana10@users.noreply.github.com>
2024-02-08 10:37:28 +0200

    Update hm5.js
    Corrections to initial code

--- homework5/hm5.js
+++ homework5/hm5.js
@@ -1,2 +1,11 @@
 let num = parseInt(prompt("Please input two-digit number"));
-document.write(String(num).split("").map(Number)[0])
+let secondDigit = num % 10
+let firstDigit = ((num - secondDigit) / 10) % 10
+
+if (firstDigit < secondDigit) {
+    document.write('The second digit is greater than first digit!')
+} else if (firstDigit > secondDigit) {
+    document.write('The first digit is greater than second digit!')
+} else if (firstDigit === secondDigit) {
+    document.write('The first digit is equal to the second digit!')
+}
