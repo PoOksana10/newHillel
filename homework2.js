dee84526bd961160178f651e7efed398e2a95b19
PoOksana10 <129370380+PoOksana10@users.noreply.github.com>
2024-02-08 10:47:40 +0200

    Update homework2.js
    Correction to initial code

--- homework2.js
+++ homework2.js
@@ -1,11 +1,14 @@
-let nmb1 = prompt('Please put something here... ');
-let nmb2 = prompt('Please put something here again... ');
-let nmb3 = prompt('Please put something here, one last time, I promise... ');
+let nmb1 = prompt('Please put some number here... ');
+let nmb2 = prompt('Please put some number here again... ');
+let nmb3 = prompt('Please put some number here, one last time, I promise... ');
 
 document.write(nmb1 + " " + nmb2 + " " + nmb3 + "<br>");
 
+let num = parseInt(prompt('Please input 5 digit number '));
+let lastDigit = num % 10;
+let fourthDigit = ((num - lastDigit) / 10) % 10;
+let thirdDigit = ((((num - lastDigit) / 10) - fourthDigit) / 10) % 10;
+let secondDigit = ((((((num - lastDigit) / 10) - fourthDigit) / 10) - thirdDigit) / 10) % 10;
+let firstDigit = parseInt(num / 10000)
 
-let num = 12345;
-let digits = num.toString().split('');
-let realDigits = digits.map(Number)
-document.write(realDigits)
+document.write(firstDigit + ' ' + secondDigit + " " + thirdDigit + " " + fourthDigit + " " + lastDigit)
