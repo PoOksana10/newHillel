332528d370ddfb341bfe4444eef91ba0b63611d9
PoOksana10 <129370380+PoOksana10@users.noreply.github.com>
2024-02-29 15:31:42 +0200

    Add files via upload

---
+++ homework16/16.js
@@ -0,0 +1,18 @@
+function generateKey(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+        counter += 1;
+    }
+    document.write(result)
+    return result;
+}
+
+console.log(generateKey(16));
+
+
+
+
+
