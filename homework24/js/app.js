e90cf590f7ec1f0766ab6812387277fbd11d59af
PoOksana10 <129370380+PoOksana10@users.noreply.github.com>
2024-03-24 23:40:05 +0200

    Update app.js

--- homework24/js/app.js
+++ homework24/js/app.js
@@ -1,10 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-  if (JSON.parse(localStorage.getItem('userList'))) {
-    localStorage.setItem('userList', JSON.stringify(users))
-  }
-  showUsers();
+    const dataBase = JSON.parse(localStorage.getItem('userList'))
+    if (dataBase.length) {
+        showUsers();
+    } else {
+        localStorage.setItem('userList', JSON.stringify(users))
+        showUsers()
+    }
 });
 
 document.querySelector('.add_btn').addEventListener('click', () => {
-  document.querySelector('#form').classList.remove('hidden');
+    document.querySelector('#info').innerHTML = ''
+    cleanElements()
+    document.querySelector('#view').classList.add('hidden');
+    document.querySelector('#form').classList.remove('hidden');
 });
+
